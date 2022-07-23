e8a2d178dcdecbbfe3fd0df1aa25959c22caa116
Taltal <taltalrealty@gmail.com>
2022-07-23 17:17:32 +0900

    .

--- config/settings.py
+++ config/settings.py
@@ -145,7 +145,7 @@ AUTH_USER_MODEL = 'users.User'
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS =  [os.path.join(BASE_DIR, "static"),]
+STATICFILES_DIRS =  [os.path.join(BASE_DIR, "static"),]
 STATIC_ROOT = os.path.join(BASE_DIR, "var", "static")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
