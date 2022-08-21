33ce44fbae125590d6e52d50671ed70024ea0b35
Taltal <taltalrealty@gmail.com>
2022-08-22 03:11:26 +0900

    .

--- config/settings.py
+++ config/settings.py
@@ -180,7 +180,7 @@ if not DEBUG:
     STATICFILES_STORAGE = 'config.custom_storages.StaticStorage'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = 'taltalshop1.6'
+    AWS_STORAGE_BUCKET_NAME = 'taltalshop16'
     AWS_DEFAULT_ACL = 'public-read'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86500'}
     AWS_S3_CUSTOM_DOMAIN = f's3.ap-northeast-2.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}'
