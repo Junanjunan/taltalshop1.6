e779513dd3cb0b32f974425fe729cf4f46167626
taltal <taltalrealty@gmail.com>
2023-10-30 14:11:58 +0900

    Modify hard coded debug settings

--- config/settings.py
+++ config/settings.py
@@ -27,8 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = DJANGO_SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+DEBUG = DEBUG_CONFIG
 
 ALLOWED_HOSTS = ["*"]
 
