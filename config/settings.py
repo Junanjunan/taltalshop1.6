2d1a2535e358720f688ed5365af030d9e66cdd21
taltal <taltalrealty@gmail.com>
2023-10-30 14:20:19 +0900

    Notify DEPLOY_URL

--- config/settings.py
+++ config/settings.py
@@ -19,6 +19,7 @@ from sentry_sdk.integrations.django import DjangoIntegration
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DEPLOY_URL = DEPLOY_URL
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
